689cdcd616773ea5e26e258d881854e71366d821
WotLote <jerab36rum@gmail.com>
2019-06-10 22:59:53 +0400

    ',,'

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const server = express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (req, res) => res.sendFile(index))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-	.use(function(req, res, next) {
+/*	.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept,application/json");
 		next();
-	})
+	})*/
 	.use(bodyParser.json())
-	.post('/auth', function(req, res){
+/*	.post('/auth', function(req, res){
 		const data = req.body;
 		connect.query('SELECT * FROM account WHERE Login LIKE "'+ data.login +'"', function(error, result, field){
 			if (error) {
@@ -32,6 +32,6 @@ const server = express()
 				}
 			}
 		})
-	})
+	})*/
 const io = socketIO(server);
 
