5e38a427aa7a101ea670ec66a9222394ca899c1a
WotLote <jerab36rum@gmail.com>
2019-06-10 22:56:59 +0400

    ',,'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const mysql = require('mysql');
-const jwt = require('jsonwebtoken');
+/*const mysql = require('mysql');
+const jwt = require('jsonwebtoken');*/
 const socketIO = require('socket.io');
 
 
@@ -10,12 +10,12 @@ const server = express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (req, res) => res.sendFile(index))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-	.use(function(req, res, next) {
+/*	.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept,application/json");
 		next();
-	})
-	.use(bodyParser.json())
+	})*/
+/*	.use(bodyParser.json())*/
 	/*.post('/auth', function(req, res){
 		const data = req.body;
 		connect.query('SELECT * FROM account WHERE Login LIKE "'+ data.login +'"', function(error, result, field){
