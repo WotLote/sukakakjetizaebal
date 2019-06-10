efb2e2e002f79b42de96fde4a8af5b63eb28951e
WotLote <jerab36rum@gmail.com>
2019-06-10 23:02:46 +0400

    'v'

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ const socketIO = require('socket.io');
 const server = express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (req, res) => res.sendFile(index))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-/*	.use(function(req, res, next) {
+	.use(bodyParser.json())
+	.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept,application/json");
 		next();
-	})*/
-	/*.use(bodyParser.json())*/
-/*	.post('/auth', function(req, res){
+	})
+	.post('/auth', function(req, res){
 		const data = req.body;
 		connect.query('SELECT * FROM account WHERE Login LIKE "'+ data.login +'"', function(error, result, field){
 			if (error) {
@@ -32,6 +31,7 @@ const server = express()
 				}
 			}
 		})
-	})*/
+	})
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 const io = socketIO(server);
 
